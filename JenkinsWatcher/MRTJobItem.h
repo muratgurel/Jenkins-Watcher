d816c3a733f6a5844f9da3ae57d7bcaa31b075a3
Murat Gurel <mrtgurel@gmail.com>
2015-01-01 20:14:46 +0200

    Custom menu item view testing.

---
+++ JenkinsWatcher/MRTJobItem.h
@@ -0,0 +1,13 @@
+//
+//  MRTJobItem.h
+//  JenkinsWatcher
+//
+//  Created by Murat Gurel on 01/01/15.
+//  Copyright (c) 2015 Murat Gurel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MRTJobItem : NSMenuItem
+
+@end
