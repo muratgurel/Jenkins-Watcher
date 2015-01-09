1faff69a04146698bbbd607bec604d0da696ed76
Murat Gurel <muratgurel@peakgames.net>
2015-01-09 17:05:27 +0200

    Removes array controller & implements context change notification for failed jobs.

--- JenkinsWatcher/MRTFailedJobsController.m
+++ JenkinsWatcher/MRTFailedJobsController.m
@@ -8,8 +8,8 @@
 
 #import "MRTFailedJobsController.h"
 #import "NSUserNotification+JobAdditions.h"
+#import <CoreData/CoreData.h>
 #import "MRTAppStatusBar.h"
-#import <AppKit/AppKit.h>
 #import "MRTJobItem.h"
 #import "MRTJob.h"
 
@@ -18,7 +18,7 @@
 @property (nonatomic, strong, readwrite) NSManagedObjectContext *context;
 @property (nonatomic, strong, readwrite) MRTAppStatusBar *statusBar;
 
-@property (nonatomic, strong) NSArrayController *jobsArrayController;
+@property (nonatomic, strong) NSMutableArray *failedJobs;
 
 @property (nonatomic) BOOL isFirstTimeFetch;
 
@@ -35,15 +35,21 @@
         _statusBar = statusBar;
         _isFirstTimeFetch = YES;
         
-        _jobsArrayController = [[NSArrayController alloc] init];
-        [_jobsArrayController setManagedObjectContext:context];
-        [_jobsArrayController setEntityName:@"Job"];
-        [_jobsArrayController setFetchPredicate:[NSPredicate predicateWithFormat:@"status == %i", JobStatusFailed]];
-        [_jobsArrayController addObserver:self forKeyPath:@"arrangedObjects" options:NSKeyValueObservingOptionNew context:NULL];
-        [_jobsArrayController setSortDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]]];
-        [_jobsArrayController fetch:self];
+        NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Job"];
+        [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"status == %i", JobStatusFailed]];
+        [fetchRequest setSortDescriptors:[[self class] sortDescriptors]];
+        
+        NSArray *results = [self.context executeFetchRequest:fetchRequest error:nil];
+        if (results.count > 0) {
+            _failedJobs = [results mutableCopy];
+            [self updateStatusBar];
+        }
+        else {
+            _failedJobs = [NSMutableArray array];
+        }
         
         [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:self];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(contextDidChangeObjects:) name:NSManagedObjectContextObjectsDidChangeNotification object:self.context];
     }
     return self;
 }
@@ -52,50 +58,76 @@
     [[NSUserNotificationCenter defaultUserNotificationCenter] setDelegate:nil];
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath
-                      ofObject:(id)object
-                        change:(NSDictionary *)change
-                       context:(void *)context {
-    if ([keyPath isEqualToString:@"arrangedObjects"]) {
-        [self.statusBar clearJobItems];
-        
-        if (!self.isFirstTimeFetch) {
-            // TODO: Present notifications
-//            NSArray *removedJobs = [notification.userInfo objectForKey:kRemovedJobsKey];
-//            NSArray *insertedJobs = [notification.userInfo objectForKey:kInsertedJobsKey];
-//            
-//            for (MRTJob *job in removedJobs) {
-//                [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:[NSUserNotification normalNotificationWithJob:job]];
-//            }
-//            
-//            for (MRTJob *job in insertedJobs) {
-//                [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:[NSUserNotification failedNotificationWithJob:job]];
-//            }
-        }
-        else {
-            self.isFirstTimeFetch = NO;
-        }
-        
-        for (MRTJob *job in [self.jobsArrayController arrangedObjects]) {
-            MRTJobItem *item = [[MRTJobItem alloc] initWithJob:job];
-            [item setTarget:self];
-            [item setAction:@selector(handleJobItemClick:)];
-            
-            [self.statusBar addJobMenuItem:item];
-        }
-        
-        if ([[self.jobsArrayController arrangedObjects] count] > 0) {
-            [self.statusBar setIconColor:StatusIconColorRed];
-        }
-        else {
-            [self.statusBar setIconColor:StatusIconColorBlack];
-        }
+- (void)handleJobItemClick:(MRTJobItem*)item {
+    [[NSWorkspace sharedWorkspace] openURL:[item.job url]];
+}
 
+- (void)updateStatusBar {
+    [self.statusBar clearJobItems];
+    
+    for (MRTJob *job in self.failedJobs) {
+        MRTJobItem *item = [[MRTJobItem alloc] initWithJob:job];
+        [item setTarget:self];
+        [item setAction:@selector(handleJobItemClick:)];
+        
+        [self.statusBar addJobMenuItem:item];
+    }
+    
+    if ([self.failedJobs count] > 0) {
+        [self.statusBar setIconColor:StatusIconColorRed];
+    }
+    else {
+        [self.statusBar setIconColor:StatusIconColorBlack];
     }
 }
 
-- (void)handleJobItemClick:(MRTJobItem*)item {
-    [[NSWorkspace sharedWorkspace] openURL:[item.job url]];
+- (void)presentUserNotificationForNewFailedJobs:(NSArray*)newFailedJobs
+                                   andFixedJobs:(NSArray*)fixedJobs {
+    if (!self.isFirstTimeFetch) {
+        for (MRTJob *job in fixedJobs) {
+            [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:[NSUserNotification normalNotificationWithJob:job]];
+        }
+        
+        for (MRTJob *job in newFailedJobs) {
+            [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:[NSUserNotification failedNotificationWithJob:job]];
+        }
+    }
+    else {
+        self.isFirstTimeFetch = NO;
+    }
+}
+
+#pragma mark - NSManagedObjectContext Notification
+
+- (void)contextDidChangeObjects:(NSNotification*)notification {
+    NSArray *insertedObjects = [[notification.userInfo objectForKey:NSInsertedObjectsKey] allObjects];
+    NSArray *updatedObjects = [[notification.userInfo objectForKey:NSUpdatedObjectsKey] allObjects];
+    
+    NSPredicate *jobPredicate = [NSPredicate predicateWithFormat: @"class == %@", [MRTJob class]];
+    NSPredicate *failedJobPredicate = [NSPredicate predicateWithFormat: @"class == %@ && status == %i", [MRTJob class], JobStatusFailed];
+    
+    NSArray *newFailedJobs = [insertedObjects filteredArrayUsingPredicate:failedJobPredicate];
+    NSMutableArray *fixedJobs = [NSMutableArray array];
+    
+    NSArray *updatedJobs = [updatedObjects filteredArrayUsingPredicate:jobPredicate];
+    for (MRTJob *job in updatedJobs) {
+        NSDictionary *changedValues = [job changedValues];
+        if ([changedValues objectForKey:@"status"]) {
+            JobStatus oldStatus = [[changedValues objectForKey:@"status"] intValue];
+            if (oldStatus == JobStatusFailed && job.status == JobStatusStable) {
+                [fixedJobs addObject:job];
+            }
+        }
+    }
+    
+    NSMutableSet *failedJobsSet = [NSMutableSet setWithArray:self.failedJobs];
+    [failedJobsSet minusSet:[NSSet setWithArray:fixedJobs]];
+    [failedJobsSet unionSet:[NSSet setWithArray:newFailedJobs]];
+    self.failedJobs = [[failedJobsSet allObjects] mutableCopy];
+    [self.failedJobs sortUsingDescriptors:[[self class] sortDescriptors]];
+    
+    [self updateStatusBar];
+    [self presentUserNotificationForNewFailedJobs:newFailedJobs andFixedJobs:[fixedJobs copy]];
 }
 
 #pragma mark - User Notification Delegate
@@ -114,6 +146,17 @@
         MRTJob *job = (MRTJob*)[self.context objectWithID:objectID];
         [[NSWorkspace sharedWorkspace] openURL:job.url];
     }
+}
+
+#pragma mark - Helpers
+
++ (NSArray*)sortDescriptors {
+    static NSArray *_sortDescriptors = nil;
+    if (!_sortDescriptors) {
+        _sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
+    }
+    
+    return _sortDescriptors;
 }
 
 @end
